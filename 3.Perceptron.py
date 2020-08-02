0764801fbb97297816325bf195dfa224f5a1b5d6
tej1996nitrr <tejaskanikdaley1996@gmail.com>
2020-08-02 12:03:01 +0530

    Yelp-vocabulary

--- 3.Perceptron.py
+++ 3.Perceptron.py
@@ -1,3 +1,42 @@
 #%%
+'''Classifying Yelp Reviews'''
 
+# %%
+from argparse import Namespace
+from collections import Counter
+import  json
+import os
+import re
+import string
+
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+from tqdm import tqdm_notebook
+
+# %%
+class Vocablulary(object):
+    """Class to process text and extract vocabulary for mapping"""
+    
+    def __init__(self, token_to_idx=None, add_unk=True, unk_token="<UNK>"):
+        """
+        Args:
+            token_to_idx (dict): a pre-existing map of tokens to indices
+            add_unk (bool): a flag that indicates whether to add the UNK token
+            unk_token (str): the UNK token to add into the Vocabulary
+        """
+        if token_to_idx is None:
+            token_to_idx = {}
+        self._token_to_idx = token_to_idx
+        self._idx_to_token = {idx: token 
+                              for token, idx in self._token_to_idx.items()}
+        self._add_unk = add_unk
+        self._unk_token = unk_token
         
+        self.unk_index = -1
+        if add_unk:
+            self.unk_index = self.add_token(unk_token) 
