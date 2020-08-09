350c678508f21448684cb29b94ab96d7cfb9e4a6
tej1996nitrr <tejaskanikdaley1996@gmail.com>
2020-08-09 23:12:34 +0530

    decoder

--- Seq2Seq/1.Seq2Seq.py
+++ Seq2Seq/1.Seq2Seq.py
@@ -60,6 +60,23 @@ class Encoder(nn.Module):
 
 
 class Decoder(nn.Module):
-    pass
+    def __init__(self, input_size, embedding_size, hidden_size, num_layers, p):
+        super(Decoder, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.dropout = nn.Dropout(p)
+        self.embedding = nn, nn.Embedding(input_size, embedding_size)
+        self.rnn = nn.LSTM(embedding_size, hidden_size, num_layers, p)
+        self.fc = nn.Linear(hidden_size, output_size)
+    
+    def forward(self, x,hidden, cell):
+        #shape of x :(N) but we want (1,N)
+        x = x.unsqueeze(0)
+        embedding = self.dropout(self.embedding(x))
+        #embedding shape : ( 1, N, embedding_size)
+        outputs,(hidden,cell) = self.rnn(embedding, (hidden, cell))
+        #shape of outputs: (1,N, hidden_size)
+
+
 class Seq2Seq(nn.Module):
     pass
