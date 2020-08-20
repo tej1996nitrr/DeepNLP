8d7721d89dd00eb2ad1e1561a02d02a2421658ee
tej199nitrr <tejaskaikdaley1996@gmail.com>
2020-08-21 01:11:58 +0530

    dataloader, dataset

--- Chatbot/train.py
+++ Chatbot/train.py
@@ -1,6 +1,9 @@
 import json
 from nlp_utils import stemming, tokenize, bag_of_words
 import numpy as np
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader, Dataset
 
 with open('intents.json', 'r') as f:
     intents = json.load(f)
@@ -34,6 +37,26 @@ for (pattern_sentence, tag) in xy:
 
 x_train = np.array(x_train)
 y_train = np.array(y_train)
+
+
+class ChatDataset(Dataset):
+    def __init__(self):
+        self.n_samples = len(x_train)
+        self.x_data = x_train
+        self.y_data = y_train
+
+    def __getitem__(self, index):
+        return self.x_data[index], self.y_data[index]
+
+    def __len__(self):
+        return self.n_samples
+
+
+# Hyper parameters
+batch_size = 8
+
+dataset = ChatDataset()
+train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=2)
 
 if __name__ == "__main__":
     pass
