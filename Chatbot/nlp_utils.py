a8aa781ddd138ac1d95f6cb648849d5a9b154ef8
tej199nitrr <tejaskaikdaley1996@gmail.com>
2020-08-21 00:25:37 +0530

    stemming tokenizing

---
+++ Chatbot/nlp_utils.py
@@ -0,0 +1,25 @@
+import nltk
+from nltk.stem.porter import PorterStemmer
+nltk.download('punkt')
+stemmer = PorterStemmer()
+
+
+def tokenize(sentence):
+    return nltk.word_tokenize(sentence)
+
+
+def stemming(word):
+    return stemmer.stem(word.lower())
+
+
+def bag_of_words(tokenized_sentence, all_words):
+    pass
+
+
+if __name__ == "__main__":
+    tokenized = tokenize("HMmm how you doing?")
+    print(tokenized)
+    stemmed=[]
+    for w in tokenized:
+        stemmed.append(stemming(w))
+    print(stemmed)
