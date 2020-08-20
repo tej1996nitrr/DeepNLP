083a953d6969d24f27d37a3f0f919fb10a94d983
tej199nitrr <tejaskaikdaley1996@gmail.com>
2020-08-21 01:00:53 +0530

    x, y train

--- Chatbot/train.py
+++ Chatbot/train.py
@@ -1,5 +1,6 @@
 import json
-from nlp_utils import stemming, tokenize
+from nlp_utils import stemming, tokenize, bag_of_words
+import numpy as np
 
 with open('intents.json', 'r') as f:
     intents = json.load(f)
@@ -20,6 +21,19 @@ all_words = [stemming(w) for w in all_words if w not in ignore_words]
 print(all_words)
 tags = sorted(set(tags))
 print(tags)
+
+x_train = []
+y_train = []
+
+for (pattern_sentence, tag) in xy:
+    bag = bag_of_words(pattern_sentence, all_words)
+    x_train.append(bag)
+    label = tags.index(tag)
+
+    y_train.append(label)
+
+x_train = np.array(x_train)
+y_train = np.array(y_train)
 
 if __name__ == "__main__":
     pass
