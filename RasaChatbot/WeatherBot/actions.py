1e774fc2af133673290fc2a17c84ffe3f90cd1ce
tej199nitrr <tejaskanikdaley1996@gmail.com>
2020-08-23 14:36:56 +0530

    actions

--- RasaChatbot/WeatherBot/actions.py
+++ RasaChatbot/WeatherBot/actions.py
@@ -1,27 +1,28 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/core/actions/#custom-actions/
+from rasa_sdk import Action
+from rasa_sdk.events import SlotSet
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+class ActionWeather(Action):
+    def name(self):
+        return 'action_weather'
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+    def run(self, dispatcher, tracker, domain):
+        from apixu.client import ApixuClient
+        api_key = '...'  # your apixu key
+        client = ApixuClient(api_key)
+
+        loc = tracker.get_slot('location')
+        current = client.current(q=loc)
+
+        country = current['location']['country']
+        city = current['location']['name']
+        condition = current['current']['condition']['text']
+        temperature_c = current['current']['temp_c']
+        humidity = current['current']['humidity']
+        wind_mph = current['current']['wind_mph']
+
+        response = """It is currently {} in {} at the moment. The temperature is {} degrees, the humidity is {}% and the wind speed is {} mph.""".format(
+            condition, city, temperature_c, humidity, wind_mph)
+
+        dispatcher.utter_message(response)
+        return [SlotSet('location', loc)]
