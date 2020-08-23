51eab6ea6a4beade18e5417023d7d5f61bd809c3
tej199nitrr <tejaskanikdaley1996@gmail.com>
2020-08-24 00:09:26 +0530

    RNN Decoder

--- ImageCaptioning/Pytorch/models.py
+++ ImageCaptioning/Pytorch/models.py
@@ -24,4 +24,17 @@ class EncoderCNN(nn.Module):
         return self.dropout(self.relu(features))
 
 
+class DecoderRNN(nn.Module):
+    def __init__(self, embed_size, hidden_size, vocab_size, num_layers):
+        super(DecoderRNN, self).__init__()
+        self.embed = nn.Embedding(vocab_size, embed_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers)
+        self.linear = nn.Linear(hidden_size, vocab_size)  # vocab size represents each word in voabulary
+        self.dropout = nn.Dropout(p=0.5)
 
+    def forward(self, features, captions):
+        # features from CNN, captions from target captions(if teacher_forcing =1)
+        embedding = self.dropout(self.embed(captions))
+        embedding = torch.cat((features.unsqueeze(0), embedding), dim=0)
+        hiddens, _ = self.lstm(embedding)  # embedding= captions+ features from image
+        outputs = self.linear(hiddens)
