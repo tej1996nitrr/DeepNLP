b7ac4f45209808ec69f80940a133e59d1911225b
tej199nitrr <tejaskanikdaley1996@gmail.com>
2020-08-24 00:33:31 +0530

    CNN to RNN

--- ImageCaptioning/Pytorch/models.py
+++ ImageCaptioning/Pytorch/models.py
@@ -41,9 +41,31 @@ class DecoderRNN(nn.Module):
         outputs = self.linear(hiddens)
         return outputs
 
-# class CNNtoRNN(nn.Module):
-#     def __init__(self, embed_size, hidden_size, vocab_size, num_layers):
-#         super(CNNtoRNN, self).__init__()
-#         self.encoderCNN = EncoderCNN(embed_size)
-#         self.decoderRNN = DecoderRNN(embed_size, hidden_size, vocab_size, num_layers)
-#
+
+class CNNtoRNN(nn.Module):
+    def __init__(self, embed_size, hidden_size, vocab_size, num_layers):
+        super(CNNtoRNN, self).__init__()
+        self.encoderCNN = EncoderCNN(embed_size)
+        self.decoderRNN = DecoderRNN(embed_size, hidden_size, vocab_size, num_layers)
+
+    def forward(self, images, captions):
+        features = self.encoderCNN(images)
+        outputs = self.decoderRNN(features, captions)
+        return outputs
+
+    def caption_image(self, image, vocabulary, max_length=50):
+        result_caption = []
+
+        with torch.no_grad():
+            x = self.encoderCNN(image).unsqueeze(0)
+            states = None  # hidden , cell states for lstms
+
+            for _ in range(max_length):
+                hiddens, states = self.decoderRNN.lstm(x, states)
+                output = self.decoderRNN.linear(hiddens.unsqueeze(0))
+                prediction = output.argmax(1)
+                result_caption.append(prediction.item())
+                x = self.decoderRNN.embed(prediction).unsqueeze(0)
+                if vocabulary.itos[prediction.item()] == '<EOS>':
+                    break
+        return [vocabulary.itos[idx] for idx in result_caption]
