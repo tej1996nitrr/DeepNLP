63e8f853dec17f837d07e1ce22fe027fe1b55ec8
tej199nitrr <tejaskanikdaley1996@gmail.com>
2020-08-24 21:55:11 +0530

    Collate

--- ImageCaptioning/Pytorch/get_loader.py
+++ ImageCaptioning/Pytorch/get_loader.py
@@ -7,45 +7,44 @@ from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data import DataLoader, Dataset
 from PIL import Image
 
+
 # We want  to convert text -> numerical values
 # 1. We need to setup a pytorch datset to load the data
 # 2. We need a vocabulary mapping each word to index
 # 3. Setup padding of every_batch (all examples  should be of same seq_length)
 class Vocabulary:
     def __init__(self, freq_threshold):
-        self.itos = {0:"<PAD>", 1:"<SOS>", 2:"<EOS>", 3:"<UNK>"}
-        self.stoi = {"<PAD>":0,"<SOS>":1,"<EOS>":2 ,"<UNK>":3}
+        self.itos = {0: "<PAD>", 1: "<SOS>", 2: "<EOS>", 3: "<UNK>"}
+        self.stoi = {"<PAD>": 0, "<SOS>": 1, "<EOS>": 2, "<UNK>": 3}
         self.freq_threshold = freq_threshold
-    
 
     def __len__(self):
         return len(self.itos)
-    
+
     @staticmethod
     def tokenize_eng(text):
         return [tok.text.lower for tok in spacy_eng.tokenizer(text)]
-    
+
     def build_vocabulary(self, sentence_list):
         frequencies = {}
         idx = 4
         for sentence in sentence_list:
             for word in self.tokenize_eng(sentence):
                 if word not in frequencies:
-                    frequenies[word]=1
+                    frequenies[word] = 1
                 else:
-                    frequencies[word]+=1
-                if frequencies[word]== self.freq_threshold:
+                    frequencies[word] += 1
+                if frequencies[word] == self.freq_threshold:
                     self.stoi[word] = idx
-                    self.itos[idx]=word
-                    idx+=1
-    
-    def numercalize(self, text):
-        tokenized_text =  self.tokenize_eng(text)
+                    self.itos[idx] = word
+                    idx += 1
+
+    def numericalize(self, text):
+        tokenized_text = self.tokenize_eng(text)
         return [
             self.stoi[token] if token in self.stoi else self.stoi["<UNK>"]
             for token in tokenized_text
         ]
-
 
 
 class FlickerDataset(Dataset):
@@ -72,9 +71,31 @@ class FlickerDataset(Dataset):
         if self.transform is not None:
             img = self.transform(img)
         numericalized_caption = [self.vocab.stoi["<SOS>"]]
-        numericalized_caption+=self.vocab.numericalize(caption)
-        numericalized_caption.append(self.voab.stoi["<EOS>"])
-        return img,torch.tensor(numericalized_caption)
-        
+        numericalized_caption += self.vocab.numericalize(caption)
+        numericalized_caption.append(self.vocab.stoi["<EOS>"])
+        return img, torch.tensor(numericalized_caption)
 
 
+class Collate:
+    def __init__(self, pad_idx):
+        self.pad_idx = pad_idx
+
+    def __call__(self, batch):
+        imgs = [item[0].unsqueesze(0) for item in batch]
+        imgs = torch.cat(imgs, dim=0)
+        targets = [item[1] for item in batch]
+        targets = pad_sequence(targets, batch_first=False, padding_value=self.pad_idx)
+        return imgs, targets
+
+
+def get_loader(root_folder, annotation_file, transform, batch_size=32, num_workers=8, shuffle=True, pin_memory=True):
+    dataset = FlickerDataset(root_folder, annotation_file, transform=transform)
+    pad_idx = dataset.vocab.stoi["<PAD>"]
+    loader = DataLoader(dataset=dataset, batch_size=batch_size, num_workers=num_workers, shuffle=shuffle,
+                        pin_memory=pin_memory, collate_fn=Collate(pad_idx=pad_idx))
+    return loader
+
+
+data_loader = get_loader(r'F:\VSCode\DeepNLP\ImageCaptioning\flicker8k',
+                         annotation_file=r'F:\VSCode\DeepNLP\ImageCaptioning\flicker8k\captions.txt', transform=None,
+                         shuffle=True)
