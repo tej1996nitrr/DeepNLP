1afe16917469f19f819d1d02553d71a165a69257
tej1996nitrr <tejaskanikdaley1996@gmail.com>
2020-08-29 21:43:38 +0530

    loading data for test, train

--- Seq2Seq/2.MachineTranslationTensorflow.py
+++ Seq2Seq/2.MachineTranslationTensorflow.py
@@ -2,10 +2,26 @@
 '''Downloading Dataset'''
 import os
 import sys
-
-# sys.path.insert(1,'/Users/tejask/Desktop/vscode/DeepNLP/Seq2Seq/utils/imdbdataset.py')
-# from imdbdataset import maybe_download_and_extract
 import utils
+import matplotlib.pyplot as plt
+import tensorflow as tf 
+import numpy as np
+from scipy.spatial.distance import cdist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, GRU, Embedding
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
-utils.imdbdataset.maybe_download_and_extract()
+# utils.imdbdataset.maybe_download_and_extract()
+# %%
+x_train_text, y_train = utils.imdbdataset.load_data(train=True)
+x_test_text, y_test = utils.imdbdataset.load_data(train=False)
+# %%
+print("Train-set Size: ", len(x_train_text))
+print("Test-set Size: ", len(x_test_text))
+data_set = x_test_text + x_train_text
+# %%
+x_train_text[1]
+y_train[1]
 # %%
