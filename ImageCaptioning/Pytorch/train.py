db4ab1e6444605161f44aa36fca6205ef067df27
tej1996nitrr <tejaskanikdaley1996@gmail.com>
2020-09-05 14:13:46 +0530

    model, optimizer, loss criteria

--- ImageCaptioning/Pytorch/train.py
+++ ImageCaptioning/Pytorch/train.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torchvision.transforms as transforms
-from torch.utils.tensorboard import SummaryWriter
+# from torch.utils.tensorboard import SummaryWriter
 from utils import save_checkpoint, load_checkpoint
 from loader import get_loader
 from models import CNNtoRNN
@@ -28,4 +28,29 @@ def train():
     save_model = True
 
     #Hyperparameters
+    embed_size = 256
+    hidden_size = 256
+    vocab_size = len(dataset.vocab)
+    num_layers = 1
+    learning_rate = 3e-4
+    num_epochs = 10
 
+    #for tensorboard
+    # writer = SummaryWriter("runs/flickr")
+    # step = 0
+
+    # initialize model, loss etc
+    model = CNNtoRNN(embed_size, hidden_size, vocab_size, num_layers).to(device)
+    criterion = nn.CrossEntropyLoss(ignore_index=dataset.vocab.stoi["<PAD>"])
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    
+    for name, param in model.encoderCNN.inception.named_parameters():
+        if "fc.weight" in name or "fc.bias" in name:
+            param.requires_grad = True
+        else:
+            param.requires_grad = train_CNN
+
+    if load_model:
+        step = load_checkpoint(torch.load("my_checkpoint.pth.tar"), model, optimizer)
+
+    model.train()
